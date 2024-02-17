b45592e94c0f0b79e25a125cb23eddb23ae5118d
Oli Carter <olicartr@gmail.com>
2024-02-17 11:53:11 +0200

    Remove extraneous boilerplate code

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,28 +1,28 @@
-import { GeistSans } from "geist/font/sans";
-import "./globals.css";
+import { Inter } from 'next/font/google'
+import './globals.css'
 
 const defaultUrl = process.env.VERCEL_URL
   ? `https://${process.env.VERCEL_URL}`
-  : "http://localhost:3000";
+  : 'http://localhost:3000'
 
 export const metadata = {
   metadataBase: new URL(defaultUrl),
-  title: "Next.js and Supabase Starter Kit",
-  description: "The fastest way to build apps with Next.js and Supabase",
-};
+  title: 'cogarden',
+  description: 'Find space to grow',
+}
+
+const inter = Inter({ subsets: ['latin'] })
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
-    <html lang="en" className={GeistSans.className}>
-      <body className="bg-background text-foreground">
-        <main className="min-h-screen flex flex-col items-center">
-          {children}
-        </main>
+    <html lang="en" className={inter.className}>
+      <body className="bg-green-950 text-green-50">
+        <main className="min-h-screen flex flex-col">{children}</main>
       </body>
     </html>
-  );
+  )
 }
