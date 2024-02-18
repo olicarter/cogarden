88c3c46e344d644dcfa51f78b0a59643c88d2e13
Oli Carter <olicartr@gmail.com>
2024-02-18 18:25:58 +0200

    Refactor Listing component and add Avatar

--- components/Listing.tsx
+++ components/Listing.tsx
@@ -1,19 +1,18 @@
 'use client'
 
-import { useState } from 'react'
+import { ComponentPropsWithoutRef, PropsWithChildren, useState } from 'react'
 import {
-  ClockClockwise,
-  CoinVertical,
-  Drop,
-  Leaf,
-  MapPin,
-  type Icon,
+  CalendarBlank,
+  Money,
+  type Icon as PhosphorIcon,
   X,
+  Check,
 } from '@phosphor-icons/react'
 import cn from '@/utils/cn'
 import Button from '@/components/Button'
 import IconButton from './IconButton'
 import { useSwipeable } from 'react-swipeable'
+import Avatar from './Avatar'
 
 export default function Listing() {
   const [isExpanded, setIsExpanded] = useState(false)
@@ -25,15 +24,15 @@ export default function Listing() {
   return (
     <div
       className={cn(
-        'bg-green-950/90 backdrop-blur-xl duration-300 flex flex-col gap-4 overflow-hidden py-4 rounded-[32px] shrink-0 text-green-50 transition-all',
-        isExpanded && 'grow',
+        'bg-green-950/90 backdrop-blur-xl duration-300 flex flex-col overflow-hidden rounded-[32px] shrink-0 text-green-50 transition-all',
+        isExpanded ? 'h-[calc(100svh-88px)]' : 'h-[292px]',
       )}
       ref={ref}
     >
       <div
         className={cn(
-          'duration-300 flex gap-4 overflow-x-auto px-4 transition-[height]',
-          isExpanded ? 'h-48' : 'h-0',
+          'box-content duration-300 flex gap-4 overflow-x-auto p-4 shrink-0 transition-[height,margin]',
+          isExpanded ? 'h-48' : 'h-28',
         )}
         style={{ scrollbarWidth: 'none' }}
       >
@@ -44,24 +43,45 @@ export default function Listing() {
           />
         ))}
       </div>
-      <header className="flex justify-between px-4">
-        <span className="font-bold text-xl">Raised bed</span>
-        <span className="font-bold text-xl">
-          6m<sup>2</sup>
-        </span>
+      <header className="flex flex-col gap-4 px-4 pb-4">
+        <div className="flex justify-between">
+          <span className="font-bold text-xl">Pelguranna</span>
+          <span className="font-bold text-xl">
+            6m<sup>2</sup>
+          </span>
+        </div>
+        <InfoItem className="col-span-2 items-center">
+          <Avatar fullName="Artjom" size="xs" />
+          <InfoItemText>Rented by Artjom</InfoItemText>
+        </InfoItem>
       </header>
-      <div className="gap-4 grid grid-cols-2 grow auto-rows-min px-4">
-        <InfoItem icon={MapPin} text="Pelguranna 37" />
-        <InfoItem icon={ClockClockwise} text="Available now" />
-        <InfoItem icon={Drop} text="No water source" />
-        <InfoItem icon={Leaf} text="Has soil" />
-        <InfoItem
-          className="col-span-2"
-          icon={CoinVertical}
-          text="€10/month + €50 deposit"
-        />
+      <div className="duration-300 gap-4 grid grid-cols-2 grow auto-rows-min overflow-hidden px-4">
+        <InfoItem>
+          <InfoItemIcon icon={X} />
+          <InfoItemText>Shared with others</InfoItemText>
+        </InfoItem>
+        <InfoItem>
+          <InfoItemIcon icon={X} />
+          <InfoItemText>Water source</InfoItemText>
+        </InfoItem>
+        <InfoItem>
+          <InfoItemIcon icon={Check} />
+          <InfoItemText>Soil</InfoItemText>
+        </InfoItem>
+        <InfoItem>
+          <InfoItemIcon icon={Check} />
+          <InfoItemText>Storage</InfoItemText>
+        </InfoItem>
+        <InfoItem>
+          <InfoItemIcon icon={Money} />
+          <InfoItemText>€10/month</InfoItemText>
+        </InfoItem>
+        <InfoItem>
+          <InfoItemIcon icon={CalendarBlank} />
+          <InfoItemText>Available now</InfoItemText>
+        </InfoItem>
       </div>
-      <div className="flex gap-4 justify-end px-4">
+      <div className="flex gap-4 justify-end p-4 pt-0">
         <IconButton
           color="green-700"
           icon={X}
@@ -88,11 +108,16 @@ export default function Listing() {
   )
 }
 
-function InfoItem(props: { className?: string; icon: Icon; text: string }) {
-  return (
-    <div className={cn('flex gap-2', props.className)}>
-      <props.icon size={20} weight="fill" />
-      <span className="leading-5 text-sm">{props.text}</span>
-    </div>
-  )
+interface InfoItemProps extends ComponentPropsWithoutRef<'div'> {}
+
+function InfoItem({ children, className }: InfoItemProps) {
+  return <div className={cn('flex gap-2', className)}>{children}</div>
+}
+
+function InfoItemIcon({ icon: Icon }: { icon: PhosphorIcon }) {
+  return <Icon size={16} weight="bold" />
+}
+
+function InfoItemText({ children }: PropsWithChildren) {
+  return <span className="leading-4 text-sm">{children}</span>
 }
